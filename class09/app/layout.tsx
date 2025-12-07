77febade7f978cf780e9c8c44a496322fc39b76e
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-12-07 17:41:46 +0500

    Intro to SQL Model and fastapi Nextjs template

---
+++ class09/app/layout.tsx
@@ -0,0 +1,39 @@
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { cn } from "@/lib/utils";
+
+export const metadata = {
+  title: "AI SDK Python Streaming Preview",
+  description:
+    "Use the Data Stream Protocol to stream chat completions from a Python endpoint (FastAPI) and display them using the useChat hook in your Next.js application.",
+  openGraph: {
+    images: [
+      {
+        url: "/og?title=AI SDK Python Streaming Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [
+      {
+        url: "/og?title=AI SDK Python Streaming Preview",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body className={cn(GeistSans.className, "antialiased dark")}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ class09/readme.md
@@ -0,0 +1,18 @@
+## Submission Form
+<a href="https://forms.gle/RK6KPvVSeQ9eV4og8">
+Submit your Hackathon here
+</a>
+
+## Group Link
+Sunday Afternoon AI/Spec-Driven Online Hackathon
+<a href="https://chat.whatsapp.com/KoYdRzD9fUJ80m0yABrjIM">
+Join Whatsapp
+</a>
+
+## How to use Claude Code with Qwen
+<a href="https://github.com/DanielHashmi/Q4_learning/blob/main/spec-driven-development/tutorials/How%20to%20Use%20Claude%20Code%20with%20Qwen%20models%20for%20Free%20on%20Linux%20and%20macOS%20(sh%20and%20bash).md">
+Qwen with Claude code</a>
+
+
+## Only use Qwen Code
+<a href="https://github.com/QwenLM/qwen-code">Qwen Code without Claude Code</a>
