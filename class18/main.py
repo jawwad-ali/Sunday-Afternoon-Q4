2f5b027bdc4975c7c2fb719fbf59f9e190bc0f34
Ali jawwad <alijawwad001@gmail.com>
2026-02-07 23:45:49 +0500

    added helpful comments

--- class18/main.py
+++ class18/main.py
@@ -11,11 +11,13 @@ from datetime import datetime, timedelta, timezone
 app = FastAPI()
 password_hasher = PasswordHash((BcryptHasher(),))
 
+# Secret Key for the Server
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # In production, use environment variable!
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = "HS256" # JWT Algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 # Token Expiry Time
 
 # ---- OAuth2 Scheme ----
+# This will be used to extract the token from the Authorization header in protected routes
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 print("oauth2_scheme", oauth2_scheme.scheme_name)
 
